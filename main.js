4b1bc9cf58feda6f4091f0c4e3f3b87ce776b077
DaisyChepyegon <daisychepyegon@gmail.com>
2022-06-15 14:24:44 +0300

    commit tree e39f1f0aec9107367ec6463baebac8cec86323ce

--- main.js
+++ main.js
@@ -3,6 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal= document.getElementById('modal');
+ const hearts = document.querySelectorAll(".like-glyph");
+ 
+ modal.className = "hidden";
+
+  const likePost = (e) =>{ 
+
+const heart = e.target
+heart.innerText = EMPTY_HEART
+mimicServerCall()
+
+  .then( responce => {
+    (heart.innerText === EMPTY_HEART)?
+    (heart.innerText = FULL_HEART, heart.classList.add("activated-heart"))
+    :(heart.innerText = EMPTY_HEART, heart.classList.remove("activated-heart"))
+  })
+
+  .catch(error => {
+    modal.classList.remove("hidden");
+    modal.querySelector("#modal-message").textContent = error;
+    setTimeout( () => {
+      modal.classList.add("hidden")
+    }, 3000)
+  })
+  }
+
+hearts.forEach( heart => {
+heart.addEventListener("click", likePost)
+})
+
+
+
+
+
 
 
 
